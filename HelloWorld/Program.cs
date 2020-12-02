26f73a162170ee4eef5a2c9d14286127c77fde0d
Tatsiana Malinina <63168842+molliny42@users.noreply.github.com>
2020-12-03 00:22:20 +0300

    feat: changed Consol ForegroundColor

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Hello World!");
         }
     }
