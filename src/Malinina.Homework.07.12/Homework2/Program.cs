ac0dfa5026e7c46932f04cf3b5f72304407c8aba
Tatsiana Malinina <63168842+molliny42@users.noreply.github.com>
2020-12-08 01:24:31 +0300

    1. deleted old project. 2. created new project. 3. renamed project

---
+++ src/Malinina.Homework.07.12/Homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework1
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            Console.WriteLine(number);
+            SetNumber(number);
+            Console.WriteLine(number);
+        }
+
+        static void SetNumber(int a)
+        {
+            a = 5;
+        }
+    }
+}
