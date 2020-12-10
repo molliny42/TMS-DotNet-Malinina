8041a7a01351257b9ac0c501136bd834bcaf4d9e
Tatsiana Malinina <63168842+molliny42@users.noreply.github.com>
2020-12-10 12:50:27 +0300

    feat: homework1 add project DateInputeHandler

---
+++ src/Malinina.Homework.DateInputHandler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Malinina.Homework.DateInput.Handler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                
+                if (!TryGetDate(out DateTime date))
+                {
+                    Console.WriteLine("Uncorrect date");
+                }
+                else
+                {
+                    Console.WriteLine($"Day is - {date.DayOfWeek}");
+                }
+            }         
+        }
+
+        static bool TryGetDate(out DateTime date)
+        {
+            Console.WriteLine("Enter date: ");
+
+            string input = Console.ReadLine();
+
+            return DateTime.TryParse(input, out date);
+        
+        }
+    }
+}
