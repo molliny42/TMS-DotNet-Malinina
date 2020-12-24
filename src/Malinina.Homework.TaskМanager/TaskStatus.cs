a1bb99e4d609af06aadf03b38a8044a0bbbdef7e
Tatsiana Malinina <63168842+molliny42@users.noreply.github.com>
2020-12-24 19:04:55 +0300

    1. init: add project Homework.DateInputHandler 2. feat: add class "Task" to describe the task (properties ID, Description, StartDate, Deadline, Status). 3. feat: add class "Calendar" for working with tasks (add, change, delete, view all tasks). 4. feat: add a selection menu 5.  fix: processing of entered dates (Deadline cannot be more than StartDate) 6. docs: add XML-comments

---
+++ src/Malinina.Homework.TaskМanager/TaskStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Malinina.Homework.TaskМanager
+{
+    /// <summary>
+    /// Task status description class.
+    /// </summary>
+    enum TaskStatus
+    {
+        /// <summary>
+        /// To do (default).
+        /// </summary>
+        ToDo = 1,
+
+        /// <summary>
+        /// In Progress.
+        /// </summary>
+        InProgress,
+
+        /// <summary>
+        /// Done.
+        /// </summary>
+        Done,
+    }
+}
